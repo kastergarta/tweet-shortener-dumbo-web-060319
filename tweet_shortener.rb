52a0fa25d9b3c098c8b32221e02c578e00a227e1
Dennis <dennis.basyrov@gmail.com>
2019-05-23 02:28:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,21 +15,10 @@ dictionary = {
 end
 
 def word_substituter(tweet)
-  dictionary = {
-  "too" => "2",
-  "to" => "2",
-  "two" =>"2",
-  "four" => "4",
-  "for" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-}
   array = tweet.split
   array.select do |word|
     if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
+      word = dictionary[word]
     else
       word
     end
